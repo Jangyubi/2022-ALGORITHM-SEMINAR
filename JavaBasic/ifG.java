75b08355ec6ba28af10fe648528006db838398f4
DESKTOP-H5SQ67P\LG <youbinking@naver.com>
2022-06-28 19:05:46 +0900

    조건문

---
+++ JavaBasic/ifG.java
@@ -0,0 +1,29 @@
+public class ifG {
+    public static void main(String[] ar) {
+
+    //if 단일
+    if(10 > 5) {
+        System.out.println("안녕하세요");
+    }
+
+    System.out.println("안녕");
+
+    //if, else if, else
+    int a = 10;
+    if(a < 7) {
+        System.out.println("안");
+    }
+    else if(a < 15) {
+        System.out.println("녕");
+    }
+
+    // switch
+    int b = 5;
+    switch(b) {
+        case 1 :
+        System.out.println("a");
+    }
+
+    }
+    
+}
