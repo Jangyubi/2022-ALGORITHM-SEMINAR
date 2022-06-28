7a50f42fd8ccde15c1fe1273aa8521a4465a6729
DESKTOP-H5SQ67P\LG <youbinking@naver.com>
2022-06-28 19:05:28 +0900

    반복문

---
+++ JavaBasic/forG.java
@@ -0,0 +1,13 @@
+public class forG {
+    public static void main(String[] ar) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("*");
+        }
+
+        int b = 0;
+        while(b < 10) {
+            System.out.println("*");
+        }
+    }
+    
+}
